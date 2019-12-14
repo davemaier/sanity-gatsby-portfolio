7c64ad5aadd29ac2682a83476d00ce9e3d0abca3
David Maier <d.maier@flowdim.com>
2019-12-14 23:37:41 +0100

    added web manifest

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -13,6 +13,20 @@ module.exports = {
     'gatsby-plugin-postcss',
     'gatsby-plugin-react-helmet',
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `David Maier`,
+        short_name: `DMaier`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: 'gatsby-source-sanity',
       options: {
         ...clientConfig.sanity,
@@ -21,6 +35,6 @@ module.exports = {
         overlayDrafts: !isProd && token
       }
     },
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
   ]
 }
