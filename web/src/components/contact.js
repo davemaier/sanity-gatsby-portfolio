5d67300c1ce8f55f038525ed54cd012f4c716981
David Maier <d.maier@flowdim.com>
2020-01-04 21:24:05 +0100

    added netlify forms test

--- web/src/components/contact.js
+++ web/src/components/contact.js
@@ -4,10 +4,27 @@ const Contact = ({ children }) => (
     <section className="section has-background-grey-light">
         <div className={`container has-text-centered`}>
             <h1 className="is-size-2">
-                Interested? 
+                Interested?
             </h1>
-            <h2 className="is-size-5">Let's get in contact and talk about your ideas! 
+            <h2 className="is-size-5">
+                Let's get in contact and talk about your ideas!
             </h2>
+
+            <div>
+                <form name="Contact Form" method="POST" data-netlify="true">
+                    <input type="hidden" name="form-name" value="Contact Form" />
+                    <div>
+                        <label>Your Email:</label>
+                        <input type="email" name="email" />
+                    </div>
+                    <div>
+                        <label>Message:</label>
+                        <textarea name="message" />
+                    </div>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+
         </div>
     </section>
 );
